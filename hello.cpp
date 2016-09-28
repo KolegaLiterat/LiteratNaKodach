e60b08fcb3bb5c8d08df55170569cc5d011f6c44
KolegaLiterat <jawor34@gmail.com>
2016-09-28 09:22:26 +0200

    Nowe linie i grupowanie instrukcji.

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@
 
 int main()
 {
-    std::cout << "Hello WORLD";
+    std::cout << "Hello WORLD\n" << "uczę się używania\n" << "nowych linii\n" << "w książce napisali, żę mam\n" << "grupować polecenia do poszczególnych instrukcji.";
+    std::cout << "Jeżenie nie będzie nowej linii, to ten tekst i tak wyświetli się wyżej";
 }
