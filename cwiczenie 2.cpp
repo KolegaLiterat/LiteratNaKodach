a69887524d95996a6d2580f9b97711ff291e1753
KolegaLiterat <jawor34@gmail.com>
2016-09-29 18:57:25 +0200

    MÓJ PIERWSZY PRZELICZNIK! JESZCZE TROCHĘ I ZROBIĘ KALKULATOR!
    (Komentarz Żony: Jak dobrze, że nikt jeszcze nie wymyślił kalkulatora...)

---
+++ cwiczenie 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main ()
+
+{
+    int cal;
+    double centrymetry;
+    double przelicznik = 2.54;
+
+    std::cout << "Podaj wymiar w calach: ";
+    std::cin >> cal;
+
+    centrymetry = cal * przelicznik;
+
+    std::cout << "\n";
+
+    std::cout << cal << " cale to " << centrymetry << " centymetrow";
+}
