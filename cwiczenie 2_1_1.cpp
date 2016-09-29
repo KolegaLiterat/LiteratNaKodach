a552616dbbb8fa4dccd887bdb493c39ad643a7cb
KolegaLiterat <jawor34@gmail.com>
2016-09-29 23:51:13 +0200

    Zrobione według wytycznych

--- cwiczenie 2_1_1.cpp
+++ cwiczenie 2_1_1.cpp
@@ -2,13 +2,11 @@
 
 int main()
 {
-	double cal;                         //Dane wejściowe
-	       
+	double cal;									//Dane wejściowe
+	
 	std::cout << "Podaj wymiar w calach:\n? ";  //Wyświetlna delikatną prośbę o wpisanie danych
 	std::cin >> cal;                            //Złapanie danej z klawiatury
 
-    double wynik = cal * 2.54;                  //Przeliczenie
-
 	std::cout << "\n" 
-		<< cal << " cale to " << wynik << " centymetrow." << std::endl;   //Wyświetlnie wyniku
+		<< cal << " cale to " << cal * 2.54 << " centymetrow." << std::endl;   //Wyświetlnie wyniku z przeliczeniem
 }
