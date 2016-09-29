4259c681f17498dbb28207dc4a9b111dabdc3269
KolegaLiterat <jawor34@gmail.com>
2016-09-29 23:53:16 +0200

    Poprawki formatowania (tak, walczę z tymi spacjami)

--- cwiczenie 2_1_1.cpp
+++ cwiczenie 2_1_1.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	double cal;									//Dane wejściowe
+	double cal;																	//Dane wejściowe
 	
-	std::cout << "Podaj wymiar w calach:\n? ";  //Wyświetlna delikatną prośbę o wpisanie danych
-	std::cin >> cal;                            //Złapanie danej z klawiatury
+	std::cout << "Podaj wymiar w calach:\n? ";									//Wyświetlna delikatną prośbę o wpisanie danych
+	std::cin >> cal;															//Złapanie danej z klawiatury
 
 	std::cout << "\n" 
-		<< cal << " cale to " << cal * 2.54 << " centymetrow." << std::endl;   //Wyświetlnie wyniku z przeliczeniem
+		<< cal << " cale to " << cal * 2.54 << " centymetrow." << std::endl;	//Wyświetlnie wyniku z przeliczeniem
 }
