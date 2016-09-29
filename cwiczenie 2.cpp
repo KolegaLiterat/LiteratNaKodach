779ad512b60d78014aac29d727404e3ef09f45c2
KolegaLiterat <jawor34@gmail.com>
2016-09-29 23:21:19 +0200

    Zmiana ustawień IDE

--- cwiczenie 2.cpp
+++ cwiczenie 2.cpp
@@ -2,14 +2,15 @@
 
 int main()
 {
-    double cal;                         //Dane wejściowe
-    double centrymetry;                 //Do zapisania wyniku         
-    double przelicznik = 2.54;          //Przelicznik danych wejściowych
+	double cal;                         //Dane wejściowe
+	double centrymetry;                 //Do zapisania wyniku         
+	double przelicznik = 2.54;          //Przelicznik danych wejściowych
 
-    std::cout << "Podaj wymiar w calach:\n? ";  //Wyświetlna delikatną prośbę o wpisanie danych
-    std::cin >> cal;                        //Złapanie danej z klawiatury
+	std::cout << "Podaj wymiar w calach:\n? ";  //Wyświetlna delikatną prośbę o wpisanie danych
+	std::cin >> cal;                        //Złapanie danej z klawiatury
 
-    centrymetry = cal * przelicznik;        //Automagiczne przeliczenie danych
+	centrymetry = cal * przelicznik;        //Automagiczne przeliczenie danych
 
-    std::cout << "\n" << cal << " cale to " << centrymetry << " centymetrow." << std::endl;   //Wyświetlnie wyniku
+	std::cout << "\n" 
+		<< cal << " cale to " << centrymetry << " centymetrow." << std::endl;   //Wyświetlnie wyniku
 }
