a7173b4df8aa9edded5c197f5ce100c745fad058
DevilaN <devilan@o2.pl>
2016-09-30 07:44:06 +0200

    Przykład normalnego wielolinijkowego wypisywania tekstu bez wielokrotnego używania operatora <<

---
+++ multiline.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "To jest tekst wielolinijkowy.\n"
+		"Trzeba pamiętać o tym, że kompilator łączy stringi w jeden.\n"
+		"Oczywiście muszą one występować przy sobie bez żadnego operatora!\n";
+}
