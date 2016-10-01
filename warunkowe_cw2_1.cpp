607acdf2e9b75205423d88433739c8cb9ad309fd
KolegaLiterat <jawor34@gmail.com>
2016-10-01 18:23:27 +0200

    Kilka drobnych poprawek ;p

--- warunkowe_cw2_1.cpp
+++ warunkowe_cw2_1.cpp
@@ -4,24 +4,30 @@ int main()
 {
 	int ile; //instrukcja określająca liczbę gwiazdek do narysowania; nie double, bo jak Ci mam narysować 0.7 gwiazdki?
 
-	std::cout << "Ile gwiazdek ma kapitan?\n?";
+	pytanie:
+	std::cout << "\n" 
+		"Ile gwiazdek ma kapitan?\n?";
 	std::cin >> ile; //nadaje wartość wpisaną z klawiatury
 	std::cout << "\n"
 		"Rysujemy! WSZYSTKIE " << ile << std::endl;
 	
 	if(ile<0) //SPRAWDZAM CZY MI ZNOWU NIE PSUJESZ PRZYKŁADU Z KSIĄŻKI!
 	{
-		std::cout << "Nie Devi, kapitan nie bedzie mial " << ile <<  " gwiazdki...";
+		std::cout << "\n" 
+			"Nie Devi, kapitan nie bedzie mial " << ile <<  " gwiazdki..." << std::endl;
+			goto pytanie;
 	}
     else
     {
+		std::cout << "\n";
            while (ile) //instrukcja, która ma być sprawdzana
 	    {
-		    std::cout << "*"; //to, co ma być wyświetlane w określonej wartości
-		    ile=ile-1; //przerywa funkcję, poprzez zmienie wartości w ile; w przeciwnym razie będzie ciągle wyświetlało gwiazdki
+		    std::cout <<"*"; //to, co ma być wyświetlane w określonej wartości
+		    ile=ile-1;
 	    }
     }
 	//test wartości ile
+	test:
 	std::cout << "\n"
 		"A teraz ile ma wartosc " << ile << std::endl;
 }
