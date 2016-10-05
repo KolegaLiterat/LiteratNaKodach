ce0912a1da623b8df1e9f072555abccd1e05be1a
KolegaLiterat <jawor34@gmail.com>
2016-10-05 13:01:21 +0200

    1. Dodany bool. Jednak nie mam pojęcia, czy użyłem go dobrze, w książce brakuje mi dobrego przykładu. 2. Usunięte pętle. Zastąpiłem je else if - wybór wielowariantowy.

--- kasa_biuro_ver2.cpp
+++ kasa_biuro_ver2.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	double kasa, kara; //bo wypada³oby za³o¿yæ, ¿e ktoœ wpise 98.90
+	double kasa; //wypada za³o¿yæ, ¿e bêdzie wartoœæ 0.7
+	bool kara; //nie mam pojêcia, czy dobrze tego u¿y³em; w ksi¹¿ce brakuje mi dobrego przyk³adu
 
-	std::cout << "Ile kosztuje utrzymanie biura?\n? "; //wpisanie wartoœci do kasa
-	std::cin >> kasa;
+	cout << "Ile kosztuje utrzymanie biura?\n? "; //wpisanie wartoœci do kasa
+	cin >> kasa;
 	
 	/* Troche bardziej rozbudowana funkcja, takie male cwiczenie, bo chcialem sprawdzicz czy potrafie cos takiego zrobic;
 	Moja walka z tym trwala bardzo dlugo, ale nauczylem sie kliku rzeczy. Przede wszystkim tego, ze mam patrzec na to, co pisze i uklada program w sposob logiczny dlatego:*/	
 
 	if (kasa <= 0) { //sprwadzam, czy wartosc kasa jest wieksza lub rowna od zera
-		std::cout << "\n"
+		cout << "\n"
 			"Przykro mi, ale wartosci ujemnej lub zera nie wpiszesz\n"
-			"Kara musi byc..." << std::endl;
+			"Kara musi byc..." << endl;
 		kara = 1;
-	} else if (kasa == 866) { //sprawdzam, czy wartosc kasa jest rowna 866
-		std::cout << "\n"
+	} else if (kasa == 866) {
+		cout << "\n"
 			"skad masz takie informacje? O.o\n"
-			"Widziales magiczna kartke?" << std::endl;
-		kara = 0; 	
-	} else {
-		while (kasa < 866) { //dopóki wpisana wartoœæ jest mniejsza, ni¿ 866 wyœwietlany jest ten tekst
-			std::cout << "\n"
-				"Uzytkowniku! Do dobrej odpowiedzi brakuje Ci " << 866 - kasa << "." << " Kara musi byc..." << std::endl;
-			kara = 1; //dostajesz kare
-			break; //przerywam pêtlê
-		}
-		while (kasa > 866) { //dopóki wartoœæ kasa jest wiêksza od 866, wyœwietlany jest ten tekst
-			std::cout << "\n"
-				"Uzytkowniku! Jest za duzo o " << kasa - 866 << "." << " Kara musi byc..." << std::endl;
-			kara = 1;
-			break; //przerywam pêtlê
-		}
+			"Widziales magiczna kartke?" << endl;
+		kara = 0;
+	} else if (kasa < 866) { //dopóki wpisana wartoœæ jest mniejsza, ni¿ 866 wyœwietlany jest ten tekst
+		cout << "\n"
+			"Uzytkowniku! Do dobrej odpowiedzi brakuje Ci " << 866 - kasa << "." << " Kara musi byc..." << endl;
+		kara = 1; //dostajesz kare
+	} else if (kasa > 866) { //dopóki wartoœæ kasa jest wiêksza od 866, wyœwietlany jest ten tekst
+		cout << "\n"
+			"Uzytkowniku! Jest za duzo o " << kasa - 866 << "." << " Kara musi byc..." << endl;
+		kara = 1;
 	}
-	if (kara = 1) { //rezygnujê ze switcha; jakoœ nie widzê sensu w sprawdzaniu switchem prawda-fa³sz
-		std::cout << "\n"
-			"BACH! MACZUGA W GLOWE!" << std::endl;
+	
+	if (kara > 0) { //rezygnujê ze switcha; jakoœ nie widzê sensu w sprawdzaniu switchem prawda-fa³sz
+		cout << "\n"
+			"BACH! MACZUGA W GLOWE!" << endl;
 	} else {
-		std::cout << "\n"
-			"BRAWO! UCIEKASZ PRZED MACZUGA!" << std::endl;
+		cout << "\n"
+			"BRAWO! UCIEKASZ PRZED MACZUGA!" << endl;
 	}
 }
