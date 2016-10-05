6dc7bc56920e585b73882992d584e29f80fe5a97
KolegaLiterat <jawor34@gmail.com>
2016-10-05 17:39:40 +0200

    Poprawki
    1. Bool;
    2. Nowa linia

--- kasa_biuro_ver2.cpp
+++ kasa_biuro_ver2.cpp
@@ -3,36 +3,32 @@ using namespace std;
 
 int main()
 {
-	double kasa; //wypada za³o¿yæ, ¿e bêdzie wartoœæ 0.7
-	bool kara; //nie mam pojêcia, czy dobrze tego u¿y³em; w ksi¹¿ce brakuje mi dobrego przyk³adu
+	float kasa; //wypada za³o¿yæ, ¿e bêdzie wartoœæ 0.7
+	bool kara = true; //nie mam pojêcia, czy dobrze tego u¿y³em; w ksi¹¿ce brakuje mi dobrego przyk³adu
 
 	cout << "Ile kosztuje utrzymanie biura?\n? "; //wpisanie wartoœci do kasa
 	cin >> kasa;
-	
+	cout << "\n";
 	/* Troche bardziej rozbudowana funkcja, takie male cwiczenie, bo chcialem sprawdzicz czy potrafie cos takiego zrobic;
 	Moja walka z tym trwala bardzo dlugo, ale nauczylem sie kliku rzeczy. Przede wszystkim tego, ze mam patrzec na to, co pisze i uklada program w sposob logiczny dlatego:*/	
 
 	if (kasa <= 0) { //sprwadzam, czy wartosc kasa jest wieksza lub rowna od zera
-		cout << "\n"
-			"Przykro mi, ale wartosci ujemnej lub zera nie wpiszesz\n"
+		cout <<	"Przykro mi, ale wartosci ujemnej lub zera nie wpiszesz\n"
+			"Zera nie ma bo biuro zawsze coœ kosztuje, a na siebie nie zarabie... (c) Thinker"
 			"Kara musi byc..." << endl;
-		kara = 1;
+			kara;
 	} else if (kasa == 866) {
-		cout << "\n"
-			"skad masz takie informacje? O.o\n"
+		cout << "Skad masz takie informacje? O.o\n"
 			"Widziales magiczna kartke?" << endl;
-		kara = 0;
 	} else if (kasa < 866) { //dopóki wpisana wartoœæ jest mniejsza, ni¿ 866 wyœwietlany jest ten tekst
-		cout << "\n"
-			"Uzytkowniku! Do dobrej odpowiedzi brakuje Ci " << 866 - kasa << "." << " Kara musi byc..." << endl;
-		kara = 1; //dostajesz kare
+		cout <<	"Uzytkowniku! Do dobrej odpowiedzi brakuje Ci " << 866 - kasa << "." << " Kara musi byc..." << endl;
+		kara; //dostajesz kare
 	} else if (kasa > 866) { //dopóki wartoœæ kasa jest wiêksza od 866, wyœwietlany jest ten tekst
-		cout << "\n"
-			"Uzytkowniku! Jest za duzo o " << kasa - 866 << "." << " Kara musi byc..." << endl;
-		kara = 1;
+		cout <<	"Uzytkowniku! Jest za duzo o " << kasa - 866 << "." << " Kara musi byc..." << endl;
+		kara;
 	}
 	
-	if (kara > 0) { //rezygnujê ze switcha; jakoœ nie widzê sensu w sprawdzaniu switchem prawda-fa³sz
+	if (kara) { //rezygnujê ze switcha; jakoœ nie widzê sensu w sprawdzaniu switchem prawda-fa³sz
 		cout << "\n"
 			"BACH! MACZUGA W GLOWE!" << endl;
 	} else {
