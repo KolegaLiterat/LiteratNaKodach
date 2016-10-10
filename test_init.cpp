b8aa536fc43890adf1389720c7bcaace083f3a76
DevilaN <devilan@o2.pl>
2016-10-10 22:41:27 +0200

    Poprawka :)

--- test_init.cpp
+++ test_init.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-	int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o;
+	int a, b, c, d, e, f, g, h, i, j, k, l, m, n;
 
 	std::cout << "Wypisujemy syf:\na = " << a << "\nb = " << b << "\nc = " << c
 		<< "\nd = " << d << "\ne = " << e << "\nf = " << f << "\ng = " << g
 		<< "\nh = " << h << "\ni = " << i << "\nj = " << j << "\nk = " << k
-		<< "\nl = " << l << "\nm = " << m << "\nn = " << n << "\no = " << o;
+		<< "\nl = " << l << "\nm = " << m << "\nn = " << n << "\n";
 }
