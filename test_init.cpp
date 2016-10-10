de90ec9436a1be6d985138635255e71f318271b7
DevilaN <devilan@o2.pl>
2016-10-10 22:36:24 +0200

    Testujemy jakie wartosci się pojawiają jak nic nie ustawimy

---
+++ test_init.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+	int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o;
+
+	std::cout << "Wypisujemy syf:\na = " << a << "\nb = " << b << "\nc = " << c
+		<< "\nd = " << d << "\ne = " << e << "\nf = " << f << "\ng = " << g
+		<< "\nh = " << h << "\ni = " << i << "\nj = " << j << "\nk = " << k
+		<< "\nl = " << l << "\nm = " << m << "\nn = " << n << "\no = " << o;
+}
