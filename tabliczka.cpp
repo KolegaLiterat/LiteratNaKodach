407fd69667ce2b9c66502054bc345dc6a56af6f7
KolegaLiterat <jawor34@gmail.com>
2016-10-13 12:00:29 +0200

    Maszyna nie jest jeszcze w pełni przetetowana!
    Dlatego, proszę, nie ciosaj mi od razu kołków na głowie!

--- tabliczka.cpp
+++ tabliczka.cpp
@@ -5,15 +5,10 @@ int main()
 {
 	int a, b;
 	
-	a = 1;
-	b = 1;
-	
-	while (a * b < 101) {
-		for (a = 1; a < 11; a++) {
-			for (b = 1; b < 11; b++) {
-				cout << a * b << ", ";
-			}
-			cout << "\n";
+	for (a = 1; a < 11; a++) {
+		for (b = 1; b < 11; b++) {
+			cout << a * b << ", ";
 		}
-	}	
+		cout << "\n";
+	}
 }
