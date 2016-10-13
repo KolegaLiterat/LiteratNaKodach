bb0c35c952fd92a3e39503aebeb1de9fe4f4c556
KolegaLiterat <jawor34@gmail.com>
2016-10-13 08:20:00 +0200

    Poprawiona.
    Według precyzyjnych graficznych wytycznych.

--- tabliczka.cpp
+++ tabliczka.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main()
 {
 	int a, b;
-	
-	cout << "Co mnozymy? ";
-	cin >> b; 	
-	
-	for (a = 1; a < 11; a++) {
-		cout << a * b << ", "; 
-	}
+
+	while (a * b < 101) {
+		for (a = 1; a < 11; a++) {
+			for (b = 1; b < 11; b++) {
+				cout << a * b << ", ";
+			}
+			cout << "\n";
+		}
+	}	
 }
