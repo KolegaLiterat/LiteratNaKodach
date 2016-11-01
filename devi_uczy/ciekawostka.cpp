9e5ba913de94b7d94329491791c8571879ac0391
DevilaN <devilan@o2.pl>
2016-11-01 20:22:10 +0100

    Czy dodawanie działa

---
+++ devi_uczy/ciekawostka.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	if (0.1 + (0.2 + 0.3) == (0.1 + 0.2) + 0.3) {
+		cout << "Równe!";
+	} else {
+		cout << "Różne!";
+	}
+	cout << endl;
+}
