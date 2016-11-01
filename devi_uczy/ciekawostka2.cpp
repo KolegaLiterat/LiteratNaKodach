02910cfe7d44b0dc713de38b6f1c20f421d20d6c
DevilaN <devilan@o2.pl>
2016-11-01 20:31:43 +0100

    Inna ciekawostka dotycząca liczb zmiennoprzecinkowych

---
+++ devi_uczy/ciekawostka2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float h = 3.0 / 7.0;
+
+	if (h == (3.0 / 7.0)) {
+		cout << "Równe!";
+	} else {
+		cout << "Różne!";
+	}
+	cout << endl;
+}
