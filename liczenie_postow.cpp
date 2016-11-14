b6d3deeb81196d2425b7432cc1a2688b2d228495
KolegaLiterat <jawor34@gmail.com>
2016-11-14 20:24:41 +0100

    Porządki!

---
+++ liczenie_postow.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#define STO_PROCENT 100
+
+using namespace std;
+
+void posty();
+void dodawanie();
+
+int main()
+{
+	int wybor;
+	
+	cout << "Co robimy?\n"
+		"[1] Liczymy zasieg\n"
+		"[2] Dodajemy szkodnikow\n";
+	cin >> wybor;
+
+	switch (wybor) {
+		case 1:
+		posty();
+		break;
+		
+		case 2:
+		dodawanie();
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void posty()
+{
+	float post, link; //potrzebne beda jeszcze zmienne zapisujace wyniki obu obliczen; docelowo program ma zapisywac dane do pliku
+	int zasieg;
+	
+	cout << "Podaj zasieg z tego tygodnia:\n"; //kolejna funkcjonalnosc: zebranie calych zasiegow i porwnanie ich (4 tygodnie)
+	cin >> zasieg;
+	
+	cout << "Ile razy byl widziany post:\n";
+	cin >> post;
+	
+	cout << "Ile razy zostal klikniety link:\n";
+	cin >> link;
+	
+	cout << " CTR P: " << STO_PROCENT * post / zasieg << "%\n" 	//CTR P - do zapisania do zmiennej
+		<< " CTR L: " << STO_PROCENT * link / zasieg << "%\n";	//CTR L - do zapisania do zmiennej
+}
+
+void dodawanie()
+{
+	int liczba, suma;
+	
+	while (liczba != 0) {
+		cout << "Ilu ich bylo:\n";
+		cin >> liczba;
+		suma = suma + liczba;
+	}
+
+	cout << "W zeszlym tygodniu mielismy " << suma << " szkodnikow.\n"; //docelowo - zapisywanie sumy uzytkowniko w ciagu tygodnia oraz flagowanie czasu trwania eventow i analizowanie ich wplywi na przyrost uzytkownikow
+}
