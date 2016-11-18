a31c756df93c85e6ba6ed158c97c57f3f810319f
KolegaLiterat <jawor34@gmail.com>
2016-11-18 09:22:49 +0100

    Zmiany procentowe w liczbie graczy.

--- liczenie_postow.cpp
+++ liczenie_postow.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void posty();
 void dodawanie();
-//void allegro(); //potrzebuje narzedzia do zbierania i analizowania danych z Allegro; to, które dosta³em trzyma dane tylko jeden tydzieñ...
+void zmianyU();
 
 int main()
 {
@@ -13,7 +13,8 @@ int main()
 	
 	cout << "Co robimy?\n"
 		"[1] Liczymy zasieg\n"
-		"[2] Dodajemy szkodnikow\n";
+		"[2] Dodajemy szkodnikow\n"
+		"[3] Procentowa zmiana\n";
 	cin >> wybor;
 
 	switch (wybor) {
@@ -24,8 +25,13 @@ int main()
 		case 2:
 			dodawanie();
 			break;
+		case 3:
+			zmianyU();
+			break;
 		
 		default:
+			cout << "BLAD!!!";
+			return 0;
 			break;
 	}
 }
@@ -50,18 +56,31 @@ void posty()
 
 void dodawanie()
 {
-	int liczba, suma = 0;
+	int liczbaU, suma = 0;
 
 	do {	
 		cout << "Ilu ich bylo:\n";
-		cin >> liczba;
-		suma += liczba;
-	} while (liczba != 0);
+		cin >> liczbaU;
+		suma += liczbaU;
+	} while (liczbaU != 0);
 
 	cout << "W zeszlym tygodniu mielismy " << suma << " szkodnikow.\n"; //docelowo - zapisywanie sumy uzytkowniko w ciagu tygodnia oraz flagowanie czasu trwania eventow i analizowanie ich wplywi na przyrost uzytkownikow
 }
 
-void allegro()
+void zmianyU()
 {
-	//Dane do zbierania: nazwa uzytkownika, ilosc aukcji, rodzaj aukcji (zloto lub postacie), udzial w sprzedazy z ostatniego tygodnia, podejete dzialanie (wpis na liste lub ban)
+	int dzien_1, dzien_2;
+
+	cout << "Pierwszy dzien:\n";
+	cin >> dzien_1;
+	
+	cout << "Drugi dzien:\n";
+	cin >> dzien_2;
+
+	if (dzien_1 > dzien_2) {
+		cout << "Zmiana wynosi " << (dzien_1 - dzien_2) * STO_PROCENT / dzien_1 << "%.\n";
+	} else if (dzien_1 < dzien_2) {
+		cout << "Zmiana wynosi " << (dzien_2 - dzien_1) * STO_PROCENT / dzien_1 << "%.\n";
+		
+	}
 }
