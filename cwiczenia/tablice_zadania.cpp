545faaa0de8e34ff4045d922387a1d5140306c9e
KolegaLiterat <jawor34@gmail.com>
2017-01-02 13:30:25 +0100

    Cwiczenia z tablic.

--- cwiczenia/tablice_zadania.cpp
+++ cwiczenia/tablice_zadania.cpp
@@ -1,7 +1,5 @@
 /*Zestaw zadañ nie koniecznie ¿yciowych, czyli napisz program, który:
 
-3. prosi u¿ytkownika o podanie 10 liczb, po czym wypisuje te liczby w kolejnoœci od najmniejszej do najwiêkszej.
-4. j/w z tym, ¿e podaje namniejsz¹ i najwiêksz¹ liczbê w tablicy
 5. prosi o wpisanie przez u¿ytkownika 10 liczb i wypisuje która liczba ile razy wyst¹pi³a.
 6. prosi o wpisanie 10 liczb, po czym wypisuje tylko liczby parzyste.
 7. prosi o wpisanie 10 liczb i wypisuje drug¹ w kolejnoœci najwiêksz¹ liczbê.
@@ -15,14 +13,17 @@ Powy¿sze zadania proszê zrealizowaæ u¿ywaj¹c przy tym tablic. Liczby dla uproszc
 
 using namespace std;
 
+//funkcje powiazanie z zadaniami
 void zadanie_1();
 void zadanie_2();
-void zadanie_3();
-void zadanie_4();
+void zadanie_3i4(int wybor);
 void zadanie_5();
 void zadanie_6();
 void zadanie_7();
 void zadanie_8();
+
+//funkcje pomocnicze
+void sortowanie(int zadanie_3[]);
 
 int main() {
 	int wybor;
@@ -38,10 +39,10 @@ int main() {
 			zadanie_2();
 			break;
 		case 3:
-			zadanie_3();
+			zadanie_3i4(wybor);
 			break;
 		case 4:
-			//zadanie_4();
+			zadanie_3i4(wybor);
 			break;
 		case 5:
 			//zadanie_5();
@@ -62,8 +63,10 @@ int main() {
 	}
 }
 
-void zadanie_1()	//1. wczytuje 10 liczb do tablicy, po czym wypisuje wszystkie liczby zapisane w tablicy.
-{
+//1. wczytuje 10 liczb do tablicy, po czym wypisuje wszystkie liczby zapisane w tablicy.
+
+void zadanie_1()
+{	
 	int zadanie_1[ROZMIAR_1];
 	int i, liczba = 3;
 
@@ -83,7 +86,9 @@ void zadanie_1()	//1. wczytuje 10 liczb do tablicy, po czym wypisuje wszystkie l
 	}
 }
 
-void zadanie_2()	//2. prosi u¿ytkownika o podanie 7 liczb po czym wypisuje te liczby w odwrotnej kolejnoœci ni¿ je wprowadzono.
+//2. prosi u¿ytkownika o podanie 7 liczb po czym wypisuje te liczby w odwrotnej kolejnoœci ni¿ je wprowadzono.
+
+void zadanie_2()	
 {
 	int zadanie_2[ROZMIAR_2];
 	int i;
@@ -102,21 +107,63 @@ void zadanie_2()	//2. prosi u¿ytkownika o podanie 7 liczb po czym wypisuje te li
 	}
 }
 
-void zadanie_3()
+/*
+3. prosi u¿ytkownika o podanie 10 liczb, po czym wypisuje te liczby w kolejnoœci od najmniejszej do najwiêkszej.
+4. j/w z tym, ¿e podaje namniejsz¹ i najwiêksz¹ liczbê w tablicy
+*/
+
+void zadanie_3i4(int wybor)
 {
-	int zadanie_3[ROZMIAR_1]; 
+	int zadanie_3i4[ROZMIAR_1]; 
 	int i;
 	
 	cout << "Wartosci podaje uzytkownik.\n";
 	
 	for (i = 0; i < ROZMIAR_1; i++) {
 		cout << "Podaj wartosc\n";
-		cin >> zadanie_3[i];
+		cin >> zadanie_3i4[i];
 	}
 	
 	cout << "Poszukiwania najmniejszej wartosci.\n";
 	
-	for (i = 0; i < ROZMIAR_1; i++) {
-		cout << i << " wartosc w tablicy to " << zadanie_3[i] << "\n";
-	}	
+	sortowanie(zadanie_3i4);
+	
+	switch (wybor) {
+		case 3:
+			for (i = 0; i < ROZMIAR_1; i++) {
+				cout << i << " wartosc w tablicy to " << zadanie_3i4[i] << "\n";
+			}
+			break;
+		case 4:
+			cout << "Najmniejsza wartosc w tablicy to " << zadanie_3i4[ROZMIAR_1 - ROZMIAR_1] << "\n"
+				<< "Najwiêszka wartoœæ w tablicy to " << zadanie_3i4[ROZMIAR_1 - 1] << "\n";
+			break;
+		default:
+			break;
+	}
+}
+
+void sortowanie(int zadanie_3i4[])
+{
+	static	int a = 0, b = 1;
+	int c;	
+
+	while (a < ROZMIAR_1) {
+		if (zadanie_3i4[a] <= zadanie_3i4[b] && b < ROZMIAR_1) {
+			b++;
+
+			sortowanie(zadanie_3i4);
+		} else if (zadanie_3i4[a] >= zadanie_3i4[b] && b < ROZMIAR_1) {
+			c = zadanie_3i4[a];
+			zadanie_3i4[a] = zadanie_3i4[b];
+			zadanie_3i4[b] = c;
+		
+			sortowanie(zadanie_3i4);
+		} else if (b == ROZMIAR_1) {
+			a++;
+			b = a + 1;		
+		
+			sortowanie(zadanie_3i4);
+		}
+	}
 }
