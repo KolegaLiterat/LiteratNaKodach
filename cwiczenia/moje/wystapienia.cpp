a962a5fbfb25a177fdbd8afe3657467d199ebc40
KolegaLiterat <jawor34@gmail.com>
2017-01-04 18:51:39 +0100

    Kolejna rozkmina

---
+++ cwiczenia/moje/wystapienia.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+#define ROZMIAR 6
+
+using namespace std;
+
+void sortowanie(int tablica[]);
+void wystapienia(int tablica[]);
+
+int main()
+{
+	int tablica[ROZMIAR] = {1, 1, 5, 6, 1, 6};
+	
+	cout << "Mysle nad wystapieniami...";
+
+	sortowanie(tablica);
+}
+
+void sortowanie(int tablica[])
+{
+	int a = 0, b = 1, c;
+
+	while (a < ROZMIAR) {
+		if (tablica[a] <= tablica[b] && b < ROZMIAR) {
+			b++;
+		} else if (tablica[a] >= tablica[b] && b < ROZMIAR) {
+			c = tablica[a];
+			tablica[a] = tablica[b];
+			tablica[b] = c;
+		} else if (b == ROZMIAR) {
+			a++;
+			b = a + 1;		
+		}
+	}
+	
+	wystapienia(tablica);
+}
+
+void wystapienia(int tablica[])
+{
+	int ile[ROZMIAR];
+	
+	int a = 0, b = 1, i; 
+	
+	for (i = 0; i < ROZMIAR; i++) {
+		ile[i] = 1;
+	}
+
+	/*while (a < ROZMIAR) {
+		if (tablica[a] == tablica[b] && b < ROZMIAR) {
+			ile[a]++;
+			b++;
+			
+		} else if (tablica[a] != tablica[b] && b < ROZMIAR) {
+			b++;
+			
+		} else if (b == ROZMIAR) {
+			a++;
+			b = a + 1;
+		}
+	}*/
+
+	for (a = 0; a < ROZMIAR; a++) {
+		if (tablica[a] != tablica[a+1] && ile[a] != ile[a + 1]) {
+			cout << "Wartosc " << tablica[a] << " wystapila " << ile[a] << " razy\n";
+		}
+	}
+		
+}
