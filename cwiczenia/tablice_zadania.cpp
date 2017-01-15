803f2686eb64d693ea2927d3979933fa372fd93b
KolegaLiterat <jawor34@gmail.com>
2017-01-15 16:03:35 +0100

    TABLICE ZADANIA
    Wprowadzenie liczenia wystapien do glownego programu

--- cwiczenia/tablice_zadania.cpp
+++ cwiczenia/tablice_zadania.cpp
@@ -1,6 +1,5 @@
 /*Zestaw zadañ nie koniecznie ¿yciowych, czyli napisz program, który:
 
-5. prosi o wpisanie przez u¿ytkownika 10 liczb i wypisuje która liczba ile razy wyst¹pi³a.
 6. prosi o wpisanie 10 liczb, po czym wypisuje tylko liczby parzyste.
 7. prosi o wpisanie 10 liczb i wypisuje drug¹ w kolejnoœci najwiêksz¹ liczbê.
 8. wczytuje liczby do macierzy 3x3 i po wszystkim wypisuje macierz.
@@ -16,14 +15,14 @@ using namespace std;
 //funkcje powiazanie z zadaniami
 void zadanie_1();
 void zadanie_2();
-void zadanie_3i4(int wybor);
-void zadanie_5();
+void zadanie_3i4i5(int wybor);
 void zadanie_6();
 void zadanie_7();
 void zadanie_8();
 
 //funkcje pomocnicze
-void sortowanie(int zadanie_3i4[]);
+void sortowanie(int zadanie_3i4i5[]);
+void wystepowanie (int zadanie_3i4i5[]);
 
 int main() {
 	int wybor;
@@ -39,13 +38,13 @@ int main() {
 			zadanie_2();
 			break;
 		case 3:
-			zadanie_3i4(wybor);
+			zadanie_3i4i5(wybor);
 			break;
 		case 4:
-			zadanie_3i4(wybor);
+			zadanie_3i4i5(wybor);
 			break;
 		case 5:
-			//zadanie_5();
+			zadanie_3i4i5(wybor);
 			break;
 		case 6:
 			//zadanie_6();
@@ -110,54 +109,74 @@ void zadanie_2()
 /*
 3. prosi u¿ytkownika o podanie 10 liczb, po czym wypisuje te liczby w kolejnoœci od najmniejszej do najwiêkszej.
 4. j/w z tym, ¿e podaje namniejsz¹ i najwiêksz¹ liczbê w tablicy
+5. prosi o wpisanie przez u¿ytkownika 10 liczb i wypisuje która liczba ile razy wyst¹pi³a.
+
 */
 
-void zadanie_3i4(int wybor)
+void zadanie_3i4i5(int wybor)
 {
-	int zadanie_3i4[ROZMIAR_1]; 
+	int zadanie_3i4i5[ROZMIAR_1]; 
 	int i;
 	
 	cout << "Wartosci podaje uzytkownik.\n";
 	
 	for (i = 0; i < ROZMIAR_1; i++) {
 		cout << "Podaj wartosc\n";
-		cin >> zadanie_3i4[i];
+		cin >> zadanie_3i4i5[i];
 	}
 	
-	cout << "Poszukiwania najmniejszej wartosci.\n";
+	cout << "Rusza sortowanie!\n";
 	
-	sortowanie(zadanie_3i4);
+	sortowanie(zadanie_3i4i5);
 	
 	switch (wybor) {
 		case 3:
 			for (i = 0; i < ROZMIAR_1; i++) {
-				cout << i << " wartosc w tablicy to " << zadanie_3i4[i] << "\n";
+				cout << i << " wartosc w tablicy to " << zadanie_3i4i5[i] << "\n";
 			}
 			break;
 		case 4:
 			//WA¯NE: Zastosowa³em tutaj sortownie od NAJMNIEJSZE do NAJWIÊKSZEJ wartoœci. Zmiana systemu sortowania wymaga zmiany komunikatu lub indeksów tablicy.
-			cout << "Najmniejsza wartosc w tablicy to " << zadanie_3i4[ROZMIAR_1 - ROZMIAR_1] << "\n"
-				<< "Najwiêszka wartoœæ w tablicy to " << zadanie_3i4[ROZMIAR_1 - 1] << "\n";
+			cout << "Najmniejsza wartosc w tablicy to " << zadanie_3i4i5[ROZMIAR_1 - ROZMIAR_1] << "\n"
+				<< "Najwiêszka wartoœæ w tablicy to " << zadanie_3i4i5[ROZMIAR_1 - 1] << "\n";
 			break;
+		case 5:
+			wystepowanie(zadanie_3i4i5);
 		default:
 			break;
 	}
 }
 
-void sortowanie(int zadanie_3i4[])
+//funkcje pomocnicze
+
+void sortowanie(int zadanie_3i4i5[])
 {
 	int a = 0, b = 1, c;
 
 	while (a < ROZMIAR_1) {
-		if (zadanie_3i4[a] <= zadanie_3i4[b] && b < ROZMIAR_1) {
+		if (zadanie_3i4i5[a] <= zadanie_3i4i5[b] && b < ROZMIAR_1) {
 			b++;
-		} else if (zadanie_3i4[a] >= zadanie_3i4[b] && b < ROZMIAR_1) {
-			c = zadanie_3i4[a];
-			zadanie_3i4[a] = zadanie_3i4[b];
-			zadanie_3i4[b] = c;
+		} else if (zadanie_3i4i5[a] >= zadanie_3i4i5[b] && b < ROZMIAR_1) {
+			c = zadanie_3i4i5[a];
+			zadanie_3i4i5[a] = zadanie_3i4i5[b];
+			zadanie_3i4i5[b] = c;
 		} else if (b == ROZMIAR_1) {
 			a++;
 			b = a + 1;		
 		}
 	}
 }
+void wystepowanie(int zadanie_3i4i5[])
+{
+	int i, wyst = 1;
+	
+	for (i = 0; i < ROZMIAR_1; i++) {
+		if (zadanie_3i4i5[i] == zadanie_3i4i5[i + 1]) {
+			wyst++;
+		} if (zadanie_3i4i5[i] != zadanie_3i4i5[i + 1]) {
+			cout << "Liczba " << zadanie_3i4i5[i] << " wystapila " << wyst << " razy.\n";
+			wyst = 1;
+		} 
+	}
+}
+
