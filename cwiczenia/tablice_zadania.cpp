0c962e67868f5ef21e56b6875203b9d762d0f44f
KolegaLiterat <jawor34@gmail.com>
2017-01-15 16:57:30 +0100

    TABLICE ZADANIE
    Dodanie wypisywania drugiej najwiekszej liczny w kolejnosci.

--- cwiczenia/tablice_zadania.cpp
+++ cwiczenia/tablice_zadania.cpp
@@ -1,6 +1,6 @@
 /*Zestaw zadañ nie koniecznie ¿yciowych, czyli napisz program, który:
 
-6. prosi o wpisanie 10 liczb, po czym wypisuje tylko liczby parzyste.
+
 7. prosi o wpisanie 10 liczb i wypisuje drug¹ w kolejnoœci najwiêksz¹ liczbê.
 8. wczytuje liczby do macierzy 3x3 i po wszystkim wypisuje macierz.
 
@@ -15,14 +15,14 @@ using namespace std;
 //funkcje powiazanie z zadaniami
 void zadanie_1();
 void zadanie_2();
-void zadanie_3i4i5(int wybor);
+void zadanie_3i4i5i7(int wybor);
 void zadanie_6();
 void zadanie_7();
 void zadanie_8();
 
 //funkcje pomocnicze
-void sortowanie(int zadanie_3i4i5[]);
-void wystepowanie (int zadanie_3i4i5[]);
+void sortowanie(int zadanie_3i4i5i7[]);
+void wystepowanie (int zadanie_3i4i5i7[]);
 
 int main() {
 	int wybor;
@@ -38,19 +38,19 @@ int main() {
 			zadanie_2();
 			break;
 		case 3:
-			zadanie_3i4i5(wybor);
+			zadanie_3i4i5i7(wybor);
 			break;
 		case 4:
-			zadanie_3i4i5(wybor);
+			zadanie_3i4i5i7(wybor);
 			break;
 		case 5:
-			zadanie_3i4i5(wybor);
+			zadanie_3i4i5i7(wybor);
 			break;
 		case 6:
-			//zadanie_6();
+			zadanie_6();
 			break;
 		case 7:
-			//zadanie_7();
+			zadanie_3i4i5i7(wybor);
 			break;
 		case 8:
 			//zadanie_8();
@@ -113,70 +113,94 @@ void zadanie_2()
 
 */
 
-void zadanie_3i4i5(int wybor)
+void zadanie_3i4i5i7(int wybor)
 {
-	int zadanie_3i4i5[ROZMIAR_1]; 
+	int zadanie_3i4i5i7[ROZMIAR_1]; 
 	int i;
 	
 	cout << "Wartosci podaje uzytkownik.\n";
 	
 	for (i = 0; i < ROZMIAR_1; i++) {
 		cout << "Podaj wartosc\n";
-		cin >> zadanie_3i4i5[i];
+		cin >> zadanie_3i4i5i7[i];
 	}
 	
 	cout << "Rusza sortowanie!\n";
 	
-	sortowanie(zadanie_3i4i5);
+	sortowanie(zadanie_3i4i5i7);
 	
 	switch (wybor) {
 		case 3:
 			for (i = 0; i < ROZMIAR_1; i++) {
-				cout << i << " wartosc w tablicy to " << zadanie_3i4i5[i] << "\n";
+				cout << i << " wartosc w tablicy to " << zadanie_3i4i5i7[i] << "\n";
 			}
 			break;
 		case 4:
 			//WA¯NE: Zastosowa³em tutaj sortownie od NAJMNIEJSZE do NAJWIÊKSZEJ wartoœci. Zmiana systemu sortowania wymaga zmiany komunikatu lub indeksów tablicy.
-			cout << "Najmniejsza wartosc w tablicy to " << zadanie_3i4i5[ROZMIAR_1 - ROZMIAR_1] << "\n"
-				<< "Najwiêszka wartoœæ w tablicy to " << zadanie_3i4i5[ROZMIAR_1 - 1] << "\n";
+			cout << "Najmniejsza wartosc w tablicy to " << zadanie_3i4i5i7[ROZMIAR_1 - ROZMIAR_1] << "\n"
+				<< "Najwiêszka wartoœæ w tablicy to " << zadanie_3i4i5i7[ROZMIAR_1 - 1] << "\n";
 			break;
 		case 5:
-			wystepowanie(zadanie_3i4i5);
+			wystepowanie(zadanie_3i4i5i7);
+			break;
+		case 7:
+			cout << "Druga w kolejnosci najwieksza wartosc w tablicy to " << zadanie_3i4i5i7[ROZMIAR_1 - 2] << "\n";
+			break;
 		default:
 			break;
 	}
 }
 
+//6. prosi o wpisanie 10 liczb, po czym wypisuje tylko liczby parzyste.
+
+void zadanie_6()
+{
+	int zadanie_6[ROZMIAR_1];
+	int i;
+	
+	for (i = 0; i < ROZMIAR_1; i++) {
+		cout << "Podaj wartosc do zapisania w tablicy:\n";
+		cin >> zadanie_6[i];
+	}
+	
+	cout << "A teraz wyswietlna siê wylacznie wartosci parzyste:\n";
+	
+	for (i = 0; i < ROZMIAR_1; i++) {
+		if (zadanie_6[i] % 2 == 0) {
+			cout << zadanie_6[i] << "\n";
+		}
+	}
+}
+
 //funkcje pomocnicze
 
-void sortowanie(int zadanie_3i4i5[])
+void sortowanie(int zadanie_3i4i5i7[])
 {
 	int a = 0, b = 1, c;
 
 	while (a < ROZMIAR_1) {
-		if (zadanie_3i4i5[a] <= zadanie_3i4i5[b] && b < ROZMIAR_1) {
+		if (zadanie_3i4i5i7[a] <= zadanie_3i4i5i7[b] && b < ROZMIAR_1) {
 			b++;
-		} else if (zadanie_3i4i5[a] >= zadanie_3i4i5[b] && b < ROZMIAR_1) {
-			c = zadanie_3i4i5[a];
-			zadanie_3i4i5[a] = zadanie_3i4i5[b];
-			zadanie_3i4i5[b] = c;
+		} else if (zadanie_3i4i5i7[a] >= zadanie_3i4i5i7[b] && b < ROZMIAR_1) {
+			c = zadanie_3i4i5i7[a];
+			zadanie_3i4i5i7[a] = zadanie_3i4i5i7[b];
+			zadanie_3i4i5i7[b] = c;
 		} else if (b == ROZMIAR_1) {
 			a++;
 			b = a + 1;		
 		}
 	}
 }
-void wystepowanie(int zadanie_3i4i5[])
+void wystepowanie(int zadanie_3i4i5i7[])
 {
 	int i, wyst = 1;
 	
 	for (i = 0; i < ROZMIAR_1; i++) {
-		if (zadanie_3i4i5[i] == zadanie_3i4i5[i + 1]) {
+		if (zadanie_3i4i5i7[i] == zadanie_3i4i5i7[i + 1]) {
 			wyst++;
-		} if (zadanie_3i4i5[i] != zadanie_3i4i5[i + 1]) {
-			cout << "Liczba " << zadanie_3i4i5[i] << " wystapila " << wyst << " razy.\n";
+		} if (zadanie_3i4i5i7[i] != zadanie_3i4i5i7[i + 1]) {
+			cout << "Liczba " << zadanie_3i4i5i7[i] << " wystapila " << wyst << " razy.\n";
 			wyst = 1;
 		} 
 	}
 }
-
