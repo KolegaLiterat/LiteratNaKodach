96510d3ca12d2f4e3c0d9a762edda022b3508569
KolegaLiterat <jawor34@gmail.com>
2017-01-31 22:03:54 +0100

    TABLICA TABLIC
    Wyswietlenie tablicy w macierzy.

--- cwiczenia/moje/tablica_tablic_cwiczenie.cpp
+++ cwiczenia/moje/tablica_tablic_cwiczenie.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
-#define MAKS_WYBOR 1
+#define MAKS_WYBOR 2
 
-void wiersze();
+//tablica ustawiona na szytwno, dlatego wpisuje obie wartosci; moglbym wpisac tylko wartosc kolumn i tez by zadzialalo
+void wiersze(int tablica[2][4]);
+void macierz(int tablica[2][4]);
 
 int main()
 {
+	int tablica[2][4] = {0, 1, 2, 3, 4, 5, 6, 7}; //tablica ma 8 elementow, bo ma dwa wiersze i cztery kolumny, co daje 8 komorek (2 * 4 = 8);
 	int wybor;
+
 	
 	cout << "Co robimy z tablica?\n"
-		"[1] Wyswietlamy wiersze oddzielnie\n";
+		"[1] Wyswietlamy wiersze oddzielnie\n"
+		"[2] Wyswietlamy macierz\n";
 	cin >> wybor;
 	
 	if (wybor < 1 || wybor > MAKS_WYBOR) {
@@ -19,7 +24,10 @@ int main()
 	} else {
 		switch (wybor) {
 			case 1:
-				wiersze();
+				wiersze(tablica);
+				break;
+			case 2:
+				macierz(tablica);
 				break;
 			default:
 				return 0;
@@ -27,9 +35,8 @@ int main()
 		}
 	}
 }
-void wiersze()
+void wiersze(int tablica[2][4])
 {
-	int tablica[2][4] = {0, 1, 2, 3, 4, 5, 6, 7}; //tablica ma 8 elementow, bo ma dwa wiersze i cztery kolumny, co daje 8 komorek (2 * 4 = 8);
 	int i, j;
 	
 	cout << "Wiersz 0\n";	
@@ -44,4 +51,18 @@ void wiersze()
 		cout << "Wartosc tablicy w adresie " << i << ", " << j << " wynosi " << tablica[i][j] << "\n";
 	}
 
+}
+void macierz(int tablica[2][4]) 
+{
+	int i, j;
+	
+	cout << "Wyswietlenie tablicy w formie macierzy\n";
+
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 4; j++) {
+			cout << "| " << tablica[i][j] << " |";
+		}
+		cout << "\n";	
+	}
+		
 }
