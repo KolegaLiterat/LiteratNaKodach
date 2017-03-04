3951a650ef6bd6e93c754df85459223fcff34d07
KolegaLiterat <jawor34@gmail.com>
2017-03-04 19:49:05 +0100

    CWICZENIE
    Zapisywanie slowa w tablicy char od tylu.

---
+++ cwiczenia/moje/odwracanie.cpp
@@ -0,0 +1,37 @@
+//ZADANIE: Stworz funkcje, ktora bedzie zapisywana podane slowo od tylu.
+
+#include <iostream>
+
+using namespace std;
+
+void odwracanie(char slowo[]);
+
+int main(int argc, char *argv[])
+{
+	char slowo[80];
+	int i;
+	
+	cout << "Wpisz slowo:\n";
+	cin >> slowo;			//Dziala na "slowa". Wpisanie "czlowiek jezdzi", spowoduje, ze w tablicy pojawi sie "czlowiek", natomiast to, co jest po spacji - wyleci w kosmos
+	
+	odwracanie(slowo);
+}
+void odwracanie(char slowo[])
+{
+	int i = 0;
+	int *wsk;
+
+	while (slowo[i] != 0) {
+		i++;
+	}
+	
+	wsk = new int (i - 1);
+	
+	cout << "Do tablicy zostalo wpisane slowo " << slowo << " a zapisanie odwrotnie wyglada tak:\n";
+	
+	for (i = *wsk; i > -1; i--) {
+		cout << slowo[i];
+	}
+	
+	delete wsk;
+}
