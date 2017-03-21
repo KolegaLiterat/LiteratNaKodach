4b38503db17621f234b1623af3ff366c3744f10f
KolegaLiterat <jawor34@gmail.com>
2017-03-21 19:59:20 +0100

    WSTAWIANIE ZNAKU
    Ukonczony program.

--- cwiczenia/moje/znaki_pomiedzy_znakami.cpp
+++ cwiczenia/moje/znaki_pomiedzy_znakami.cpp
@@ -1,5 +1,7 @@
+//dołączanie bibliotek
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -45,11 +47,40 @@ void sztywna_tablica()
     
     rozmiar = strlen(tekst);
 
+    cout << "Rozmiar to " << rozmiar << "\n";
+
     for (i = 0; i <= rozmiar; i++) {
         cout << "-" << tekst[i];
     }
 }
+//dodowanie wpisanego przez uzytkownika znaku pomiedzy znaki w dowolnej tresci
 void podawanie_tresci()
 {
-    cout << "PUSTO I HULA CYFROWY WIATR!\n";
+    //stringi
+    string miasto;
+    //zmienne
+    int rozmiar, i;
+    char znak;
+    //wskazniki
+    char *tekst;
+
+    //wpisywanie tekstu; uzywam getline(), poniewaz cin ucina przy bialym znaku; pierwsz getline() zjada biale znaki, zeby drugi mogl wystartowac;
+    cout << "Wpisz dowolnie dlugi tekst\n";
+    getline(cin, miasto);
+    getline(cin, miasto);
+
+    cout << "Jaki znak ma rozdzielac inne znaki w tresci?\n";
+    cin >> znak;
+
+    //sprawdzenie dlugosci stringa i zapisanie go w zmiennej rozmiar
+    rozmiar = miasto.length();
+    //stworzenie tablicy char na podstawie dlugosci; +1 bo potrzebne jest mi miejsce na null;
+    tekst = new char [rozmiar + 1];
+    //przekopiowanie wartosci stringa do tablicy char;
+    strcpy(tekst, miasto.c_str());
+
+    //petla wpisujaca znaki
+    for (i = 0; i <= rozmiar; i++) {
+        cout << znak << tekst[i];
+    }
 }
