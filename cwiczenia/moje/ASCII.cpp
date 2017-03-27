561032947da7eb087a072afa9dddd4f2f5ea7092
KolegaLiterat <jawor34@gmail.com>
2017-03-27 21:17:59 +0200

    ASCII
    1. Zakomentowanie dwowch petli.
    2. Poprawienie warunku wyswietlania znakow spoza zbioru.

--- cwiczenia/moje/ASCII.cpp
+++ cwiczenia/moje/ASCII.cpp
@@ -17,7 +17,8 @@ int main()
    cout << "Wpisz tekst:\n";
    getline(cin, tekst);
    
-   cout << "1. Wyswietlnie kodow ASCII poszczegolnych znakow:\n";
+   ////////////////////////////////////////////////////////////////
+   /*cout << "1. Wyswietlnie kodow ASCII poszczegolnych znakow:\n";
 
    for (i = 0; i < tekst.length(); i++) {
        cout << "-" << (int)tekst[i] << "-";
@@ -26,14 +27,15 @@ int main()
    cout << "\n2. Przesuniecie kodow ASCII o trzy pozycje w zbiorze:\n";
 
    for (i = 0; i < tekst.length(); i++) {
-       if ((int)tekst[i] == 32) {
+       if ((int)tekst[i] < (int)'A' || (int)tekst[i] > 'Z') {
            cout << "-" << (int)tekst[i] << "-";
        } else {
            cout << "-" << (int)'A' + ((int)tekst[i] - (int)'A' + 3) % ((int)'Z' - (int)'A' + 1) << "-";
        }
-   }
+   }*/
+   ////////////////////////////////////////////////////////////////
 
-   cout << "\n3. Wyswietlenie przesunietych liter na podstawie kodow ASCII:\n";
+   cout << "Wyswietlenie przesunietych liter na podstawie kodow ASCII:\n";
 
    for (i = 0; i < tekst.length(); i++) {
        cout << "-" << Fzmiana_znaku(&tekst[i]) << "-";
@@ -43,8 +45,8 @@ char Fzmiana_znaku(string znak)
 {
 	int n_kod;
 
-    if ((int)znak[0] == 32) {
-        n_kod = 32;
+    if ((int)znak[0] < (int)'A' || (int)znak[0] > 'Z') {
+        n_kod = (int)znak[0];
     } else {
 	    n_kod = (int)'A' + ((int)znak[0] - (int)'A' + 3) % ((int)'Z' - (int)'A' + 1);
     }
