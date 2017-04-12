5c37a69da1730d295a518ff789a0b5a5ab5eaf43
KolegaLiterat <jawor34@gmail.com>
2017-04-12 15:33:18 +0200

    PLANSZA
    Modyfikacja petli tworzacej pokoje.

--- cwiczenia/moje/plaszowka/planszowka.cpp
+++ cwiczenia/moje/plaszowka/planszowka.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void generowanie_planszy();
+int losowanie_znaku();
 
 int main()
 {
@@ -12,7 +13,9 @@ int main()
 void generowanie_planszy()
 {
     char plansza[6][10];
-    int i, j;
+    bool pole;
+    int i, j, znak;
+    int test = 0, pozycja = 1; 
 
     for (i = 0; i < 6; i++) {
         if (i == 0 || i == 5) {
@@ -37,9 +40,28 @@ void generowanie_planszy()
 
     for (i = 1; i < 5; i++) {
         for (j = 1; j < 8; j++) {
+            znak = losowanie_znaku();
 
+            switch (znak) {
+                case 0:
+                    plansza[i][j] = ' ';
+                    break;
+                
+                case 1:
+                    if (plansza[i][j - 1] == ' ' && plansza[i][j + 1] == ' ' && plansza[i - 1][j] == '#' && plansza[i + 1][j] == ' ') {
+                        plansza[i][j] = '#';
+                    } else if (plansza[i][j - 1] == '#' && plansza[i][j + 1] == ' ' && plansza[i - 1][j] == ' ' && plansza[i + 1][j] == ' ') {
+                        plansza[i][j] = '#';
+                    } else if (plansza[i][j - 1] == '#' && plansza[i][j + 1] == ' ' && plansza[i - 1][j] == '#' && plansza[i - 1][j - 1] == ' ' && plansza[i + 1][j] == ' ') {
+                        plansza[i][j] = '#';
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
-    } 
+    }
 
     for (i = 0; i < 6; i++) {
        for (j = 0; j < 10; j++) {
@@ -48,4 +70,12 @@ void generowanie_planszy()
 
        cout << "\n";
     }
+}
+int losowanie_znaku()
+{
+    int znak;
+
+    znak = (rand() % 2) + 0;
+
+    return znak;
 }
