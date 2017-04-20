fbe81825851fc463092ea2056d1ebb152a83e820
KolegaLiterat <jawor34@gmail.com>
2017-04-20 19:37:57 +0200

    PLANSZA
    Dodanie warunkow wpywajacych na rozprzestrzenianie sie wydarzenia (ogien)
    na planszy.

--- cwiczenia/moje/plaszowka/planszowka.cpp
+++ cwiczenia/moje/plaszowka/planszowka.cpp
@@ -99,6 +99,7 @@ void wyswietlenie_planszy(char plansza[][10])
         }
        cout << "\n";
     }
+    cout << "\n";
 }
 void losowanie_wydarzen(char plansza[][10])
 {
@@ -122,56 +123,94 @@ void losowanie_wydarzen(char plansza[][10])
 }
 void tura(char plansza[][10])
 {
-    static int tura = 1;
+    static int powtorzenia = 1;
     int i, j;
     char dym = 's', ogien = 'F', ponownie;
 
-    if (tura % 2 = 1) {
+    if (powtorzenia % 2 == 1) {
         for (i = 0; i < 6; i++) {
             for (j = 0; j < 10; j++) {
-                if (plansza[i][j] == ogien) {
-                    plansza[i - 1][j] = dym;
+                if (i == 0) {
+                    if (plansza[i][j] == ogien && j == 0) {
+                        plansza[i][j + 1] = dym;
+                        plansza[i + 1][j] = dym;
+                    } else if (plansza[i][j] == ogien && j == 9) {
+                        plansza[i][j - 1] = dym;
+                        plansza[i + 1][j] = dym;
+                    } else if (plansza[i][j] == ogien) {
+                        plansza[i][j + 1] = dym;
+                        plansza[i][j - 1] = dym;
+                        plansza[i + 1][j] = dym;
+                    } 
+                } else if (i == 6) {
+                    if (plansza[i][j] == ogien && j == 0) {
+                        plansza[i][j + 1] = dym;
+                        plansza[i - 1][j] = dym;
+                    } else if (plansza[i][j] == ogien && j == 9) {
+                        plansza[i][j - 1] = dym;
+                        plansza[i - 1][j] = dym;
+                    } else if (plansza[i][j] == ogien) {
+                        plansza[i][j + 1] = dym;
+                        plansza[i][j - 1] = dym;
+                        plansza[i - 1][j] = dym;
+                    }
+                } else if (plansza[i][j] == ogien) {
+                    plansza[i][j + 1] = dym;
+                    plansza[i][j - 1] = dym;
                     plansza[i + 1][j] = dym;
-                    plansza[i][j + 1] = dym;
-                    plansza[i][j + 1] = dym;
-                } else if (plansza[i][j] == ogien && i == 0) {
-                    if (j == 0) {
-                        plansza[i + 1][j] = dym;
-                        plansza[i][j + 1] = dym;
-                    } else if (j == 9) {
-                        plansza[i + 1][j] = dym;
-                        plansza[i][j - 1] = dym;
-                    } else {
-                        plansza[i + 1][j] = dym;
-                        plansza[i][j + 1] = dym;
-                        plansza[i][j - 1] = dym;
+                    plansza[i - 1][j] = dym;
+                }
+            }
+        }
+    } else if (powtorzenia % 2 == 0) {
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j < 10; j++) {
+                if (i == 0) {
+                    if (plansza[i][j] == dym && j == 0) {
+                        plansza[i][j + 1] = ogien;
+                        plansza[i + 1][j] = ogien;
+                    } else if (plansza[i][j] == dym && j == 9) {
+                        plansza[i][j - 1] = ogien;
+                        plansza[i + 1][j] = ogien;
+                    } else if (plansza[i][j] == dym) {
+                        plansza[i][j + 1] = ogien;
+                        plansza[i][j - 1] = ogien;
+                        plansza[i + 1][j] = ogien;
+                    } 
+                } else if (i == 6) {
+                    if (plansza[i][j] == dym && j == 0) {
+                        plansza[i][j + 1] = ogien;
+                        plansza[i - 1][j] = ogien;
+                    } else if (plansza[i][j] == dym && j == 9) {
+                        plansza[i][j - 1] = ogien;
+                        plansza[i - 1][j] = ogien;
+                    } else if (plansza[i][j] == dym) {
+                        plansza[i][j + 1] = ogien;
+                        plansza[i][j - 1] = ogien;
+                        plansza[i - 1][j] = ogien;
                     }
-                } else if (plansza[i][j] == ogien && i == 5) {
-                    if (j == 0) {
-                        plansza[i - 1][j] = dym;
-                        plansza[i][j + 1] = dym;
-                    } else if (j == 9) {
-                        plansza[i - 1][j] = dym;
-                        plansza[i][j - 1] = dym;
-                    } else {
-                        plansza[i - 1][j] = dym;
-                        plansza[i][j + 1] = dym;
-                        plansza[i][j - 1] = dym;
-                    }
+                } else if (plansza[i][j] == dym) {
+                    plansza[i][j + 1] = ogien;
+                    plansza[i][j - 1] = ogien;
+                    plansza[i + 1][j] = ogien;
+                    plansza[i - 1][j] = ogien;
                 }
             }
         }
     }
 
-    for (i = 0; i < 6; i++) {
-        for (j = 0; j < 10; j++) {
-            if (plansza[i][j] == ogien) {
-                plansza[i][j + 1] = dym;
-            //} else if (plansza[i][j] == dym) {
-            //    plansza[i][j] = ogien;
-            }
-        }
-    }
-
     wyswietlenie_planszy(plansza);
+
+    cout << "Nastepna tura?\n[T]ak lub [N]ie\n";
+    cin >> ponownie;
+
+    if (ponownie == 'T' || ponownie == 't') {
+        powtorzenia++;
+
+        tura(plansza);
+    } else if (ponownie == 'N' || ponownie == 'n') {
+        cout << "No to koniec!\n";
+    } else {
+        cout << "Blad! A po bledzie - koniec ;(\n";
+    }
 }
