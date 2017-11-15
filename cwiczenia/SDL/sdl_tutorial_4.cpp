0dccde6a90a8df69538c18db168c670b08ca7475
KolegaLiterat <jawor34@gmail.com>
2017-11-15 18:56:25 +0100

    SDL_Rendery
    Dodanie dwoch viewportów

--- cwiczenia/SDL/sdl_tutorial_4.cpp
+++ cwiczenia/SDL/sdl_tutorial_4.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 //screen resolution
-const int SCREEN_WIDTH = 1440;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 //SDL init and close
 bool sdl_init();
@@ -40,9 +40,24 @@ int main(int argc, char *argv[])
                         quit = true;
                     }
                 }
-                
                 SDL_RenderClear(gRenderTutorial);
+
+                SDL_Rect leftviewport;
+                leftviewport.x = 0;
+                leftviewport.y = 0;
+                leftviewport.w = SCREEN_WIDTH / 2;
+                leftviewport.h = SCREEN_HEIGHT;
+                SDL_RenderSetViewport(gRenderTutorial, &leftviewport);
                 SDL_RenderCopy(gRenderTutorial, gTextureTutorial, NULL, NULL);
+
+                SDL_Rect rightviewport;
+                rightviewport.x = SCREEN_WIDTH / 2;
+                rightviewport.y = 0;
+                rightviewport.w = SCREEN_WIDTH / 2;
+                rightviewport.h = SCREEN_HEIGHT;
+                SDL_RenderSetViewport(gRenderTutorial, &rightviewport);
+                SDL_RenderCopy(gRenderTutorial, gTextureTutorial, NULL, NULL);
+
                 SDL_RenderPresent(gRenderTutorial);
             }
         }
@@ -58,21 +73,21 @@ bool sdl_init()
         success = false;
     } else {
         gTutorial4 = SDL_CreateWindow("Rendery!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
-        
+
         if (gTutorial4 == NULL) {
             cout << "Blad inicjalizacji okna! BLAD: " << SDL_GetError() << "\n";
             success = false;
         } else {
             gRenderTutorial = SDL_CreateRenderer(gTutorial4, -1, SDL_RENDERER_ACCELERATED);
-            
+
             if (gRenderTutorial == NULL) {
                 cout << "Blad inicjalizacji rendera! BLAD: " << SDL_GetError() << "\n";
                 success = false;
             } else {
                 int imgFlags = IMG_INIT_PNG;
-                
+
                 SDL_SetRenderDrawColor(gRenderTutorial, 0xFF, 0xFF, 0xFF, 0xFF);
-                
+
                 if (!(IMG_Init(imgFlags) &imgFlags)) {
                     cout << "Blad inicjalizacji obslugi tekstur! BLAD: " << SDL_GetError() << "\n";
                     success = false;
