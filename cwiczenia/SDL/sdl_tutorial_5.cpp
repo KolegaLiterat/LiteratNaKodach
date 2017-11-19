87bfd6bc5285948820de15d249c45ae284928b4c
KolegaLiterat <jawor34@gmail.com>
2017-11-19 23:33:28 +0100

    RENDERY
    Praca na spritesheetach.

--- cwiczenia/SDL/sdl_tutorial_5.cpp
+++ cwiczenia/SDL/sdl_tutorial_5.cpp
@@ -3,12 +3,31 @@
 #include <string>
 #include <SDL.h>
 #include <SDL_image.h>
-#include "Ltexture.h"
 
 using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+class LTexture 
+{
+    public:
+    
+    
+    LTexture();
+    ~LTexture();
+    bool load_file(string path);
+    void free();
+    void render(int x, int y, SDL_Rect *clip = NULL);
+    int get_width();
+    int get_height();
+
+    private:
+
+    SDL_Texture *htexture;
+    int hwidth;
+    int hheight;
+};
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 bool sdl_init();
 bool load_graphic();
@@ -16,8 +35,12 @@ void sdl_close();
 
 SDL_Window *gTutorial5 = NULL;
 SDL_Renderer *gRenderTutorial = NULL;
-LTexture gFrontTexture;
-LTexture gBackTexutre;
+//LTexture gFrontTexture;
+//LTexture gBackTexutre;
+
+//sprite
+SDL_Rect gSprites[4];
+LTexture gSpriteSheet;
 
 int main(int argc, char *argv[])
 {
@@ -31,17 +54,21 @@ int main(int argc, char *argv[])
             cout << "Blad inicjalizacji obslug grafiki! BLAD: " << SDL_GetError() << "\n";
         } else {
             while (!quit) {
-                while (SDL_PoolEvent(&event) != 0) {
+                while (SDL_PollEvent(&event) != 0) {
                     if (event.type == SDL_QUIT) {
                         quit = true;
                     } 
                 }
                 
                 SDL_SetRenderDrawColor(gRenderTutorial, 0xFF, 0xFF, 0xFF, 0xFF);
-                SDL_RenderClear(gRender);
-                
-                gBackTexutre.render(0, 0);
-                gFrontTexture.render(0, 0);
+                SDL_RenderClear(gRenderTutorial);
+               
+                gSpriteSheet.render(0, 0, &gSprites[0]);
+                gSpriteSheet.render(SCREEN_WIDTH - gSprites[1].w, 0, &gSprites[1]);
+                gSpriteSheet.render(0, SCREEN_HEIGHT - gSprites[2].h, &gSprites[2]);
+                gSpriteSheet.render(SCREEN_WIDTH - gSprites[3].w, SCREEN_HEIGHT - gSprites[3].h, &gSprites[3]);
+                //gBackTexutre.render(0, 0);
+                //gFrontTexture.render(240, 190);
 
                 SDL_RenderPresent(gRenderTutorial);
             }
@@ -54,17 +81,17 @@ bool sdl_init()
 {
     bool success = true;
 
-    if (SDL_Init(SDL_Init_Video) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "Blad inicjalizacji SDL! BLAD: " << SDL_GetError() << "\n";
         success = false;
     } else {
-        gTutorial5 = SDL_CreateWindow("Rendery 2!", SDL_WINDOWPOS_UNDEFINDED, SDL_WINDOWPOS_UNDEFINDED, SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
+        gTutorial5 = SDL_CreateWindow("Rendery 2!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
 
         if (gTutorial5 == NULL) {
             cout << "Blad inicjalizacji okna! BLAD: " << SDL_GetError() << "\n";
             success = false;
         } else {
-            gRenderTutorial = SDL_CreateRender(gTutorial5, -1, SDL_RENDERER_ACCELERATED);
+            gRenderTutorial = SDL_CreateRenderer(gTutorial5, -1, SDL_RENDERER_ACCELERATED);
 
             if (gRenderTutorial == NULL) {
                 cout << "Blad inicjalizacji rendera! BLAD: " << SDL_GetError() << "\n";
@@ -72,7 +99,7 @@ bool sdl_init()
             } else {
                 int imgFlags = IMG_INIT_PNG;
 
-                SDL_RenderDrawColor(gRenderTutorial, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_SetRenderDrawColor(gRenderTutorial, 0xFF, 0xFF, 0xFF, 0xFF);
 
                 if (!(IMG_Init(imgFlags) &imgFlags)) {
                     cout << "Blad inicjalizacji obslugi tekstur! BLAD: " << SDL_GetError() << "\n";
@@ -88,22 +115,51 @@ bool load_graphic()
 {
     bool success = true;
 
-    if (!gFrontTexture.load_file(TODO: PATH)) {
+    if (!gSpriteSheet.load_file("graphic/sprites.png")) {
+        cout << "Blad ladowania sprite'ow! BLAD: " << SDL_GetError() << "\n";
+        success = false;
+    } else {
+        //gora lewo
+        gSprites[0].x = 0;
+        gSprites[0].y = 0;
+        gSprites[0].w = 100;
+        gSprites[0].h = 100;
+
+        //gora prawo
+        gSprites[1].x = 100;
+        gSprites[1].y = 0;
+        gSprites[1].w = 100;
+        gSprites[1].h = 100;
+
+        //dol lewo
+        gSprites[2].x = 0;
+        gSprites[2].y = 100;
+        gSprites[2].w = 100;
+        gSprites[2].h = 100;
+
+        //dol prawo
+        gSprites[3].x = 100;
+        gSprites[3].y = 100;
+        gSprites[3].w = 100;
+        gSprites[3].h = 100;
+    }
+
+    /*if (!gFrontTexture.load_file("graphic/front.png")) {
         cout << "Blad ladowania pierwszej tekstury!\n";
         success = false;
     }
 
-    if (!gBackTexutre.load_file(TODO: PATH)) {
+    if (!gBackTexutre.load_file("graphic/back.png")) {
         cout << "Blad ladowania tla!a\n";
         success = false;
-    }
+    }*/
 
     return success;
 }
 void sdl_close()
 {
-    gFrontTexture.free();
-    gBackTexutre.free();
+    //gFrontTexture.free();
+    //gBackTexutre.free();
 
     SDL_DestroyRenderer(gRenderTutorial);
     gRenderTutorial = NULL;
@@ -112,4 +168,71 @@ void sdl_close()
 
     IMG_Quit();
     SDL_Quit();
+}
+//LTEXTURE
+LTexture::LTexture()
+{
+    htexture = NULL;
+    hwidth = 0;
+    hheight = 0;
+}
+LTexture::~LTexture()
+{
+    free();
+}
+bool LTexture::load_file(string path)
+{
+    SDL_Texture *newtexture = NULL;
+    SDL_Surface *loadsurface = NULL;
+
+    free();
+
+    loadsurface = IMG_Load(path.c_str());
+
+    if (loadsurface == NULL) {
+        cout << "Blad tworzenie powierzchi! BLAD: " << SDL_GetError() << " Sciezka: " << path << "\n";
+    } else {
+        SDL_SetColorKey(loadsurface, SDL_TRUE, SDL_MapRGB(loadsurface->format, 0, 0xFF, 0xFF));
+        newtexture = SDL_CreateTextureFromSurface(gRenderTutorial, loadsurface);
+        
+        if (newtexture == NULL) {
+            cout << "Blad inicjalizacji tekstury! BLAD:" << SDL_GetError() << " Sciezka: " << path << "\n";
+        } else {
+            hwidth = loadsurface->w;
+            hheight = loadsurface->h;
+        }
+
+        SDL_FreeSurface(loadsurface);
+    }
+
+    htexture = newtexture;
+    return htexture != NULL;
+}
+void LTexture::free()
+{
+    if (htexture != NULL) {
+        SDL_DestroyTexture(htexture);
+        htexture = NULL;
+        hwidth = 0;
+        hheight = 0;
+    }
+}
+void LTexture::render(int x, int y, SDL_Rect *clip)
+{
+    SDL_Rect renderquad = {x, y, hwidth, hheight};
+    
+    if (clip != NULL) {
+        renderquad.w = clip->w;
+        renderquad.h = clip->h;
+    }
+    
+    SDL_RenderCopy(gRenderTutorial, htexture, clip, &renderquad);
+}
+int LTexture::get_width()
+{
+    return hwidth;
+}
+int LTexture::get_height()
+{
+    return hheight;
 }
