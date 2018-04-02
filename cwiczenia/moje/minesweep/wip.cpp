69ff1a38e78e39a5cf87dc191385782b17b6a347
KolegaLiterat <jawor34@gmail.com>
2018-04-02 20:41:09 +0200

    M

--- cwiczenia/moje/minesweep/wip.cpp
+++ cwiczenia/moje/minesweep/wip.cpp
@@ -1,14 +1,43 @@
 #include <cstdlib>
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    char board[11]; //2D board array
-
+    //2D board array
+    char board[8][11]; 
+    
     //board elements
     char empty = '0', mine = '*', wall = '+';
-}
+    
+    //control variables
+    int i = 0, j = 0;
 
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 11; j++) {
+            if (i == 0 || i == 7) {
+                board[i][j] = wall;
+            } else if (i == 1 && j == 0 || i == 6 && j == 10) {
+                board[i][j] = wall;
+            } else if (i > 1 && i < 6 && j == 0 || i > 1 && i < 6 && j == 10) {
+                board[i][j] = wall;
+            } else if (i == 2 && j == 7 || i == 4 && j == 5 || i == 4 && j == 7) {
+                board[i][j] = mine;
+            } else {
+                board[i][j] = empty;
+            }
+        }
+    }
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 11; j++) {
+            cout << board[i][j];
+        }
+        cout << "\n";
+    }
+}
+/**
 +++++++++++
 +0000000000
 +000000*00+
@@ -17,3 +46,4 @@ int main()
 +000000000+
 M000*00000+
 +++++++++++
+**/
