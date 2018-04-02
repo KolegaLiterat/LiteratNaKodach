b59d52ec0e46f025d2acdaa239ebfcaa2958e6a9
KolegaLiterat <jawor34@gmail.com>
2018-04-02 19:18:23 +0200

    MINEROBOT - board elements

---
+++ cwiczenia/moje/minesweep/wip.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    char board[11]; //2D board array
+
+    //board elements
+    char empty = '0', mine = '*', wall = '+';
+}
+
++++++++++++
++0000000000
++000000*00+
++000000000+
++000*00*00+
++000000000+
+M000*00000+
++++++++++++
