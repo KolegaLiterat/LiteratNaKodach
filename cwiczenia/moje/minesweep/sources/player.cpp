2a774e7c3a6ee3134f48e103794efee16986d024
KolegaLiterat <jawor34@gmail.com>
2018-04-05 15:18:48 +0200

    Few experiments

--- cwiczenia/moje/minesweep/sources/player.cpp
+++ cwiczenia/moje/minesweep/sources/player.cpp
@@ -10,20 +10,24 @@ using namespace std;
 //get input & rules
 void Player::get_input()
 {
+    Player getInput;
+
     cout << "Sterowanie robotem:\n[W] -> Do gory\n[S] -> Na dol\n[A] -> W lewo\n[D] -> W prawo\n[I] -> Zaplon\n"
         << "[S] -> STOP\nZasady:\nNalezy przeprowadzic robota [R] z punktu startowego do mety [M]\n";
     cout << "Opisz ściezke!\n";
-    getline(cin, Player::input);
+    getline(cin, getInput.input);
 }
 //test input to check characters from user
 bool Player::test_input()
 {   
+    Player stringArray;
+
     bool isInputCorrect = true;
     int i, j, characterCount = 0;
 
-    for (i = 0; i < Player::input.length(); i++) {
+    for (i = 0; i < stringArray.input.length(); i++) {
         
-        for (j = 0; j < Player::correctCharacters.length(); j++) {
+        for (j = 0; j < stringArray.correctCharacters.length(); j++) {
             if (Player::input[i] != Player::correctCharacters[j]) {
                 characterCount++;
             }
