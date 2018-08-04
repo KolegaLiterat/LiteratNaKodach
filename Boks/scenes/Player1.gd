90d00b6637a6c167971852a2ffa43ca92d955492
KolegaLiterat <jawor34@gmail.com>
2018-08-04 23:26:00 +0200

    BOKS
    hand attack definition

--- Boks/scenes/Player1.gd
+++ Boks/scenes/Player1.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export (int) var speedMovement
+export (int) var hand
 var screensize
 
 func _ready():
@@ -16,15 +17,20 @@ func _process(delta):
 	elif Input.is_action_pressed("ui_left"):
 		position.x -= speedMovement
 		$AnimatedSprite.play("walkBack")
+	elif Input.is_action_pressed("player1attack"):
+		if hand == 0:
+			hand = 1;
+			$AnimatedSprite.play("punchRight")
+		elif hand == 1:
+			hand = 0
+			$AnimatedSprite.play("punchLeft")
 	else:
 		$AnimatedSprite.play("idle")
 	
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	
-func playerAttack():
-	var 
-	
-	if Input.is_action_pressed("player1attack"):
-		$AnimatedSprite.play("
+		
+		
+		
 	
